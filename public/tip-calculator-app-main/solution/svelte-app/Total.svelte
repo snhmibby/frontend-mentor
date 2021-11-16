e742acd439057330677b9c4ee93e4d393cdaf3a4
Jur <jurjenm@yahoo.com>
2021-11-16 17:05:57 +0100

    remove public/ from git

--- public/tip-calculator-app-main/solution/svelte-app/Total.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let value;
-</script>
-
-<section class="total-section">
-	<h2><slot>Total</slot></h2>
-	<p>/ person </p>
-	<strong>${value.toFixed(2)}</strong>
-</section>
--- public/tip-calculator-app-main/solution/svelte-app/main.js
+++
@@ -1,7 +0,0 @@
-import App from './App.svelte';
-
-const app = new App({
-	target: document.querySelector("#svelte-hook"),
-});
-
-export default app;
